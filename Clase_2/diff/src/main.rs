3f2f44819eef9a4496705da560bb797210af607f
sanleomon <170890793+sanleomon@users.noreply.github.com>
2026-09-07 02:53:51 +0000

    Implement LCS grid for diff exercise

--- Clase_2/diff/src/main.rs
+++ Clase_2/diff/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use std::fs;
 use std::path::Path;
 
@@ -11,6 +12,28 @@ fn read_file_lines(ruta: &Path) -> Vec<String> {
         .collect()
 }
 
+fn lcs(
+    archivo_1: &[String],
+    archivo_2: &[String],
+) -> Vec<Vec<usize>> {
+    let m = archivo_1.len();
+    let n = archivo_2.len();
+
+    let mut grilla: Vec<Vec<usize>> = vec![vec![0; n + 1]; m + 1];
+
+    for i in 0..m {
+        for j in 0..n {
+            if archivo_1[i] == archivo_2[j] {
+                grilla[i+1][j+1] = grilla[i][j] + 1;
+            } else {
+                grilla[i+1][j+1] = cmp::max(grilla[i+1][j], grilla[i][j+1]);
+            }
+        }
+    }
+
+    grilla
+}
+
 fn main() {
     let ruta_1 = Path::new("archivos/archivo1.txt");
     let ruta_2 = Path::new("archivos/archivo2.txt");
@@ -18,6 +41,9 @@ fn main() {
     let archivo_1 = read_file_lines(ruta_1);
     let archivo_2 = read_file_lines(ruta_2);
 
+    let grilla = lcs(&archivo_1, &archivo_2);
+
     println!("{:?}", archivo_1);
     println!("{:?}", archivo_2);
+    println!("{:?}", grilla);
 }
